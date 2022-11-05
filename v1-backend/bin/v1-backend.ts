9a0284d048f7d360ca78e86fb610faf9077add22
prathik <37804760+prmali@users.noreply.github.com>
2022-11-05 02:14:13 -0700

    Update v1-backend.ts

--- v1-backend/bin/v1-backend.ts
+++ v1-backend/bin/v1-backend.ts
@@ -10,7 +10,10 @@ new V1PortifiStack(app, "DEV-PortifiStack", {
 	 * but a single synthesized template can be deployed anywhere. */
 	/* Uncomment the next line to specialize this stack for the AWS Account
 	 * and Region that are implied by the current CLI configuration. */
-	// env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+	env: {
+		account: process.env.CDK_DEFAULT_ACCOUNT,
+		region: process.env.CDK_DEFAULT_REGION,
+	},
 	/* Uncomment the next line if you know exactly what Account and Region you
 	 * want to deploy the stack to. */
 	// env: { account: '123456789012', region: 'us-east-1' },
