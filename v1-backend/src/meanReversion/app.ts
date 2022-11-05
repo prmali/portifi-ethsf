b12be95c34c9ddc04f9b7be8e5bb37fbad8ab1fa
prathik <37804760+prmali@users.noreply.github.com>
2022-11-05 06:34:19 -0700

    to-dos

--- v1-backend/src/meanReversion/app.ts
+++ v1-backend/src/meanReversion/app.ts
@@ -1,6 +1,6 @@
 import "source-map-support/register";
 
-import { getProtocols } from "../@common";
+import { getProtocols } from "../@common/LlamaClient";
 
 export const lambdaHandler = async (event, context) => {
 	return await getProtocols();
