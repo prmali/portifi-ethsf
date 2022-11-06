3a86bfe38c0aa6d30e7eee8ce7ddb2c25477ea9c
Punn Kam <punn@euler.xyz>
2022-11-06 01:55:02 -0700

    hard code

--- v1-core/scripts/MainnetFork.s.sol
+++ v1-core/scripts/MainnetFork.s.sol
@@ -20,7 +20,7 @@ contract MainnetFork is Script {
 	IUniswapV2Router02 uniswapRouter;
 
     function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        uint256 deployerPrivateKey = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
         vm.startBroadcast(deployerPrivateKey);
 
 		// Initialize
